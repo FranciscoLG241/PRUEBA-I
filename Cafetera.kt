547a9bfc2ec50a14a74111152ddce70a60cfd309
Fran <fletgas@g.educaand.es>
2025-02-05 15:58:12 +0100

    Comienzo de la clase cafetera

---
+++ Cafetera.kt
@@ -0,0 +1,21 @@
+
+import java.awt.Color
+
+class Cafetera(val ubicacion: String) {
+    private var cantidad: Int = 0
+    private val capacidadMaxima: Int = 1000
+
+
+    constructor(ubicacion: String, capacidadMaxima: Int) : this(ubicacion) {
+        this.cantidad = capacidadMaxima
+    }
+
+
+    constructor(ubicacion: String, capacidadMaxima: Int, cantidad: Int) : this(ubicacion) {
+        this.cantidad = if (cantidad > capacidadMaxima) {
+            capacidadMaxima
+        } else {
+            cantidad
+        }
+    }
+}
