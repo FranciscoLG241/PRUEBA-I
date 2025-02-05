1ef6446a8ee56cd4d2e8d2e9c625aabb897b11af
Fran <fletgas@g.educaand.es>
2025-02-05 16:59:44 +0100

    Comienzo del main con las tres cafeteras y las tazas aleatorias

--- PRUEBA.kt
+++ PRUEBA.kt
@@ -1,3 +1,8 @@
+import kotlin.random.Random
+
+
+
+
 class Cafetera(val ubicacion: String) {
     private var cantidad: Int = 0
     private val capacidadMaxima: Int = 1000
@@ -106,8 +111,52 @@ class Taza(var color: Color = Color.BLANCO, var capacidad: Int = 50){
 
 
 
+fun generarTazasAleatorias(cantidadTazas: Int): List<Taza> {
+    return List(cantidadTazas) {
+        val capacidadAleatoria = when (Random.nextInt(3)) {
+            0 -> 50
+            1 -> 75
+            else -> 100
+        }
+
+        Taza(capacidad = capacidadAleatoria)
+    }
+}
+
+
+
+
+
+
 
 fun main(){
+
+    val cafetera1 = Cafetera("Salón", 1000, 0)
+    val cafetera2 = Cafetera("Cocina", 750, 750)
+    val cafetera3 = Cafetera("Balcón", 500, 200)
+
+
+    println(cafetera1)
+    println(cafetera2)
+    println(cafetera3)
+
+
+    val tazas = generarTazasAleatorias(20)
+
+    print("\n **LISTA DE TAZAS**")
+    tazas.forEach{ println(it)}
+
+
+    println("**********************************************")
+
+
+
+
+
+
+
+
+
 
 
 
